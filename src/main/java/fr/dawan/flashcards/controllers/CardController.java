f646960f2aaf81475c3558d0f1222f1d239f8ec9
Sukushi <romain.chuche@gmail.com>
2023-12-18 16:29:05 +0100

    tabs

--- src/main/java/fr/dawan/flashcards/controllers/CardController.java
+++ src/main/java/fr/dawan/flashcards/controllers/CardController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("Cards")
 public class CardController {
-    // Je souhaite developer la fonctionnalité : NE PAS METTRE DANS LE CONTROLLER
-    /*private static boolean answerCondition;
-    public static boolean isAnsweredCorrectly(){
-        // Récupérer la réponse de l'utilisateur : Vraie ou fausse
-        // Appelle un affichage pour l'utilisateur après avoir affiché le verso
-        return answerCondition;
-    }
-    public static Card*/
+	// Je souhaite developer la fonctionnalité : NE PAS METTRE DANS LE CONTROLLER
+	/*private static boolean answerCondition;
+	public static boolean isAnsweredCorrectly(){
+		// Récupérer la réponse de l'utilisateur : Vraie ou fausse
+		// Appelle un affichage pour l'utilisateur après avoir affiché le verso
+		return answerCondition;
+	}
+	public static Card*/
 }
