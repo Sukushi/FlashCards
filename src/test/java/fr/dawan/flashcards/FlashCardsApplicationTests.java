e6fe83b951ac83f51bc16e21c3c8af66ba25358c
Sukushi <romain.chuche@gmail.com>
2023-12-22 14:07:05 +0100

    Feat : application-test

--- src/test/java/fr/dawan/flashcards/FlashCardsApplicationTests.java
+++ src/test/java/fr/dawan/flashcards/FlashCardsApplicationTests.java
@@ -2,8 +2,10 @@ package fr.dawan.flashcards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlashCardsApplicationTests {
 
 	@Test
