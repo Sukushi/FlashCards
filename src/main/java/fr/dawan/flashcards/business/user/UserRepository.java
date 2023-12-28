cc0e1147e34ed7af0ee9b5e56668c5c9d79827f2
BarbeDouce <baptiste.louerat44@gmail.com>
2023-12-28 15:36:38 +0100

    Fix JpaRepo method Add new psvm de tests

--- src/main/java/fr/dawan/flashcards/business/user/UserRepository.java
+++ src/main/java/fr/dawan/flashcards/business/user/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByEmailOrUsername(String email, String username);
+    Optional<User> findByEmailOrName(String email, String username);
 }
