7c1cccc658ed9ee2ae42facd65ba25044491d2e4
BarbeDouce <baptiste.louerat44@gmail.com>
2024-01-04 08:38:29 +0100

    Fixes : LoginDto getters and setters

--- src/main/java/fr/dawan/flashcards/business/auth/LoginDto.java
+++ src/main/java/fr/dawan/flashcards/business/auth/LoginDto.java
@@ -14,8 +14,15 @@ public class LoginDto {
     public String getEmail() {
         return email;
     }
-
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
