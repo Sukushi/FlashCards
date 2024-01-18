11a634e39d858806a769cdacc76510e6641d8889
BarbeDouce <baptiste.louerat44@gmail.com>
2024-01-18 17:01:47 +0100

    Fixes: raw DATA ok

--- src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
+++ src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
@@ -35,91 +35,98 @@ public class FlashCardsApplication implements CommandLineRunner {
 		cardRepository.save(new Card("", Category.TEST, "",""));
 		 */
 
-		if (cardRepository.findAll().size() == 0){
+		//if (cardRepository.findAll().size() == 1){
 			cardRepository.save(new Card("temoin", Category.TEST,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("allouer", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("service", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("abstraction", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("interface", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("classe", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("dependance", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("objet", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("algorithme", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("heritage", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("encapsulation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("requete", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("cyclomatique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("dette_technique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("propriete", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("configuration", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("implementation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("controller", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("reflexion", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("genericite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("package", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("invoquer", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("exceptions", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("permissif", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("statique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("specifique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("thread", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("visibilite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("restfull", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("annotation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("repository", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("audit", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("entitee", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("back-end", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("front-end", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("full-stack", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("cahier-des-charges", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("plugin", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("cardinalite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("semantique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("variables", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("variable-systeme", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("variable-environnement", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("allouer", Category.DEFINITION,"Qu'est ce que \"allouer\" signifie ?",""));
+			cardRepository.save(new Card("service", Category.DEFINITION,"Qu'est ce qu'un \"service\" ?",""));
+			cardRepository.save(new Card("abstraction", Category.DEFINITION,"Qu'est ce qu'une \"abstraction\" ?",""));
+			cardRepository.save(new Card("interface", Category.DEFINITION,"Qu'est ce qu'une \"interface\" ?",""));
+			cardRepository.save(new Card("classe", Category.DEFINITION,"Qu'est ce qu'une \"classe\" ?",""));
+			cardRepository.save(new Card("dependance", Category.DEFINITION,"Qu'est ce qu'une \"dépendance\" ?",""));
+			cardRepository.save(new Card("objet", Category.DEFINITION,"Qu'est ce qu'un \"objet\" en programmation ?",""));
+			cardRepository.save(new Card("algorithme", Category.DEFINITION,"Qu'est ce qu'un \"algorithme\" ?",""));
+			cardRepository.save(new Card("heritage", Category.DEFINITION,"Qu'est ce qu'un \"heritage\" ?",""));
+			cardRepository.save(new Card("encapsulation", Category.DEFINITION,"Qu'est ce que l' \"encapsulation\" ? Quels sont ces avantages ?",""));
+			cardRepository.save(new Card("requete", Category.DEFINITION,"Qu'est ce qu'une \"requête\" ? Donner 3 exemples.",""));
+			cardRepository.save(new Card("cyclomatique", Category.DEFINITION,"Qu'est ce que la \"complexité cyclomatique\" ?",""));
+			cardRepository.save(new Card("dette_technique", Category.DEFINITION,"Qu'est ce que la \"dette technique\" ?",""));
+			cardRepository.save(new Card("propriete", Category.DEFINITION,"Qu'est ce qu'une\"propriété\" signifie ?",""));
+			cardRepository.save(new Card("configuration", Category.DEFINITION,"Qu'est ce qu'une \"configuration\" ?",""));
+			cardRepository.save(new Card("implementation", Category.DEFINITION,"Qu'est ce qu'une \"implémentation\" ?",""));
+			cardRepository.save(new Card("controller", Category.DEFINITION,"Qu'est ce qu'un \"controller\" ?",""));
+			cardRepository.save(new Card("reflexion", Category.DEFINITION,"Qu'est ce que la \"réflexion\" signifie ?",""));
+			cardRepository.save(new Card("genericite", Category.DEFINITION,"Qu'est ce que la \"généricité\" signifie ?",""));
+			cardRepository.save(new Card("package", Category.DEFINITION,"Qu'est ce qu'un \"package\" ?",""));
+			cardRepository.save(new Card("invoquer", Category.DEFINITION,"Qu'est ce qu' \"invoquer\" signifie ?",""));
+			cardRepository.save(new Card("exceptions", Category.DEFINITION,"Qu'est ce qu'une \"exception\" ?",""));
+			cardRepository.save(new Card("permissif", Category.DEFINITION,"Qu'est ce que le terme \"permissif\" signifie ?",""));
+			cardRepository.save(new Card("statique", Category.DEFINITION,"Qu'est ce que le terme \"statique\" signifie ?",""));
+			cardRepository.save(new Card("specifique", Category.DEFINITION,"Qu'est ce que le terme \"spécifique\" signifie ?",""));
+			cardRepository.save(new Card("thread", Category.DEFINITION,"Qu'est ce qu'un \"thread\" ?",""));
+			cardRepository.save(new Card("visibilite", Category.DEFINITION,"Qu'est ce que le terme \"visibilité\" signifie ? Citez les 4 valeurs",""));
+			cardRepository.save(new Card("restfull", Category.DEFINITION,"Qu'est ce que le terme \"restfull\" signifie ?",""));
+			cardRepository.save(new Card("annotation", Category.DEFINITION,"Qu'est ce qu'une\"annotation\" ?",""));
+			cardRepository.save(new Card("repository", Category.DEFINITION,"Qu'est ce qu'un \"repository\" ?",""));
+			cardRepository.save(new Card("audit", Category.DEFINITION,"Qu'est ce qu'un \"audit\" ?",""));
+			cardRepository.save(new Card("entitee", Category.DEFINITION,"Qu'est ce qu'une \"entité\" ?",""));
 
-			cardRepository.save(new Card("framework", Category.DEFINITION, "",""));
-
-			cardRepository.save(new Card("UUID", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("DTO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("SCRUM", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("DAO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("JPA", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("GAV", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("JVM", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("POO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("DCU", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("DC", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-			cardRepository.save(new Card("JPA", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
-
-			cardRepository.save(new Card("JSON", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("XML", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("TXT", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("SQL", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("CSV", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("WAR", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-			cardRepository.save(new Card("JAR", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
-
-			cardRepository.save(new Card("trello", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("surefire", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("maven", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("eclipse", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("intellij", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("jenkins", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("sonarlint", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("sonarqube", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("postman", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
-
-			cardRepository.save(new Card("Java", Category.LANGAGE,"Citez moi trois particularités de ce langage : ","U U I D"));
-			cardRepository.save(new Card("Javascript", Category.LANGAGE,"Citez moi trois particularités de ce langage : ","U U I D"));
-
-			cardRepository.save(new Card("Angular", Category.FRAMEWORK,"Quel est la fonction de ce logiciel ?","U U I D"));
-			cardRepository.save(new Card("React", Category.FRAMEWORK,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("cahier-des-charges", Category.DEFINITION,"Qu'est ce qu'un \"cahier des charges\" ?",""));
+			cardRepository.save(new Card("plugin", Category.DEFINITION,"Qu'est ce qu'un \"plugin\" ?",""));
+			cardRepository.save(new Card("cardinalite", Category.DEFINITION,"Qu'est ce qu'une \"cardinalité\" ?",""));
+			cardRepository.save(new Card("semantique", Category.DEFINITION,"Qu'est ce que le terme \"sémantique\" signifie ?",""));
+			cardRepository.save(new Card("variable", Category.DEFINITION,"Qu'est ce qu'une \"variable\" ?",""));
+			cardRepository.save(new Card("variable-systeme", Category.DEFINITION,"Qu'est ce qu'une \"variable système\" ?",""));
+			cardRepository.save(new Card("variable-environnement", Category.DEFINITION,"Qu'est ce qu'une \"variable d'environnement\" ?",""));
+	
+			cardRepository.save(new Card("framework", Category.DEFINITION, "Qu'est ce qu'un \"framework\" ?",""));
+			
+			cardRepository.save(new Card("back-end", Category.ARCHITECTURE,"Qu'est ce que le terme \"back-end\" signifie ?",""));
+			cardRepository.save(new Card("front-end", Category.ARCHITECTURE,"Qu'est ce que le terme \"front-end\" signifie ?",""));
+			cardRepository.save(new Card("full-stack", Category.ARCHITECTURE,"Qu'est ce que le terme \"full-stack\" signifie ?",""));
+			
 
 
-		}
+			cardRepository.save(new Card("UUID", Category.SIGLE,"Que veut dire le sigle : UUID ?",""));
+			cardRepository.save(new Card("DTO", Category.SIGLE,"Que veut dire le sigle : DTO ?",""));
+			cardRepository.save(new Card("SCRUM", Category.SIGLE,"Que veut dire le sigle : SCRUM ?",""));
+			cardRepository.save(new Card("DAO", Category.SIGLE,"Que veut dire le sigle : DAO ?",""));
+			cardRepository.save(new Card("JPA", Category.SIGLE,"Que veut dire le sigle : JPA ?",""));
+			cardRepository.save(new Card("GAV", Category.SIGLE,"Que veut dire le sigle : GAV ?",""));
+			cardRepository.save(new Card("JVM", Category.SIGLE,"Que veut dire le sigle : JVM ?",""));
+			cardRepository.save(new Card("POO", Category.SIGLE,"Que veut dire le sigle : POO ?",""));
+			cardRepository.save(new Card("DCU", Category.SIGLE,"Que veut dire le sigle : DCU ?",""));
+			cardRepository.save(new Card("DC", Category.SIGLE,"Que veut dire le sigle : DC ?",""));
+			//cardRepository.save(new Card("MOT", Category.SIGLE,"Que veut dire le sigle : MOT ?",""));
+
+			cardRepository.save(new Card("JSON", Category.EXTENSION,"A quoi correspond cette extension : JSON",""));
+			cardRepository.save(new Card("XML", Category.EXTENSION,"A quoi correspond cette extension : XML",""));
+			cardRepository.save(new Card("TXT", Category.EXTENSION,"A quoi correspond cette extension : TXT",""));
+			cardRepository.save(new Card("SQL", Category.EXTENSION,"A quoi correspond cette extension : SQL",""));
+			cardRepository.save(new Card("CSV", Category.EXTENSION,"A quoi correspond cette extension : CSV",""));
+			cardRepository.save(new Card("WAR", Category.EXTENSION,"A quoi correspond cette extension : WAR",""));
+			cardRepository.save(new Card("JAR", Category.EXTENSION,"A quoi correspond cette extension : JAR",""));
+
+			cardRepository.save(new Card("trello", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Trello",""));
+			cardRepository.save(new Card("jacoco", Category.SOFTWARE,"Quel est la fonction de ce logiciel : JaCoCo",""));
+			cardRepository.save(new Card("surefire", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Surefire",""));
+			cardRepository.save(new Card("maven", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Maven",""));
+			cardRepository.save(new Card("eclipse", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Eclipse",""));
+			cardRepository.save(new Card("intellij", Category.SOFTWARE,"Quel est la fonction de ce logiciel : IntelliJ",""));
+			cardRepository.save(new Card("jenkins", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Jenkins",""));
+			cardRepository.save(new Card("sonarlint", Category.SOFTWARE,"Quel est la fonction de ce logiciel : Sonarlint",""));
+			cardRepository.save(new Card("sonarqube", Category.SOFTWARE,"Quel est la fonction de ce logiciel : SonarQube",""));
+			cardRepository.save(new Card("postman", Category.SOFTWARE,"Quel est la fonction de ce logiciel : POST Man",""));
+
+			cardRepository.save(new Card("Java", Category.LANGAGE,"Citez moi trois particularités de ce langage : Java",""));
+			cardRepository.save(new Card("Javascript", Category.LANGAGE,"Citez moi trois particularités de ce langage : Javascript",""));
+
+			cardRepository.save(new Card("Angular", Category.FRAMEWORK,"Quels sont les particularités de ce framework : Angular",""));
+			cardRepository.save(new Card("React", Category.FRAMEWORK,"Quels sont les particularités de ce framework : React",""));
+			cardRepository.save(new Card("Spring", Category.FRAMEWORK,"Quels sont les particularités de ce framework : Spring",""));
+			cardRepository.save(new Card("Symfony", Category.FRAMEWORK,"Quels sont les particularités de ce framework : Symfony",""));
+
+
+		//}
 
 
 	}
