85d6ab90c76b3271a225ab1f7608b822e306f109
BarbeDouce <baptiste.louerat44@gmail.com>
2024-01-18 15:57:39 +0100

    Fixes: App init() maj + Category update v2

--- src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
+++ src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
@@ -31,30 +31,94 @@ public class FlashCardsApplication implements CommandLineRunner {
 		// Méthode qui s'exécute avant L'application
 		System.out.println("\t>>>>>>>>> Run before App\n");
 
+		/*
+		cardRepository.save(new Card("", Category.TEST, "",""));
+		 */
+
 		if (cardRepository.findAll().size() == 0){
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("Allouer", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("Service", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
-			cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("temoin", Category.TEST,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("allouer", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("service", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("abstraction", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("interface", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("classe", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("dependance", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("objet", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("algorithme", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("heritage", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("encapsulation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("requete", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("cyclomatique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("dette_technique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("propriete", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("configuration", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("implementation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("controller", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("reflexion", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("genericite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("package", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("invoquer", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("exceptions", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("permissif", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("statique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("specifique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("thread", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("visibilite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("restfull", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("annotation", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("repository", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("audit", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("entitee", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("back-end", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("front-end", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("full-stack", Category.ARCHITECTURE,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("cahier-des-charges", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("plugin", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("cardinalite", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("semantique", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("variables", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("variable-systeme", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+			cardRepository.save(new Card("variable-environnement", Category.DEFINITION,"Question Témoin","Réponse Témoin"));
+
+
+
+			cardRepository.save(new Card("UUID", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("DTO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("SCRUM", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("DAO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("JPA", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("GAV", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("JVM", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("POO", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("DCU", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("DC", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+			cardRepository.save(new Card("JPA", Category.SIGLE,"Que veut dire ce sigle ?","U U I D"));
+
+			cardRepository.save(new Card("JSON", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("XML", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("TXT", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("SQL", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("CSV", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("WAR", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+			cardRepository.save(new Card("JAR", Category.EXTENSION,"A quoi correspond cette extension ?","U U I D"));
+
+			cardRepository.save(new Card("trello", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("surefire", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("maven", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("eclipse", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("intellij", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("jenkins", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("sonarlint", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("sonarqube", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("postman", Category.SOFTWARE,"Quel est la fonction de ce logiciel ?","U U I D"));
+
+			cardRepository.save(new Card("Java", Category.LANGAGE,"Citez moi trois particularités de ce langage : ","U U I D"));
+			cardRepository.save(new Card("Javascript", Category.LANGAGE,"Citez moi trois particularités de ce langage : ","U U I D"));
+
+			cardRepository.save(new Card("Angular", Category.FRAMEWORK,"Quel est la fonction de ce logiciel ?","U U I D"));
+			cardRepository.save(new Card("React", Category.FRAMEWORK,"Quel est la fonction de ce logiciel ?","U U I D"));
+
+
 		}
 
 
