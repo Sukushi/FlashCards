5533e8b1746ac672058b5997373bc4373d3da578
BarbeDouce <baptiste.louerat44@gmail.com>
2024-01-18 15:12:27 +0100

    Feat: MAJ de FlashCardsApplication. Add run (init)

--- src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
+++ src/main/java/fr/dawan/flashcards/FlashCardsApplication.java
@@ -1,18 +1,40 @@
 package fr.dawan.flashcards;
 
+import fr.dawan.flashcards.business.card.Card;
+import fr.dawan.flashcards.business.card.CardRepository;
+import fr.dawan.flashcards.business.card.Category;
+import fr.dawan.flashcards.business.passage.PassageRepository;
+import fr.dawan.flashcards.business.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class FlashCardsApplication {
+public class FlashCardsApplication implements CommandLineRunner {
 
+	@Autowired
+	CardRepository cardRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	PassageRepository passageRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(FlashCardsApplication.class, args);
 		// SpringApplication.run(JoeLaBidouille.class, args);
 		// SpringApplication.run(RoronoahSansChaussures.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Méthode qui s'exécute avant L'application
+		System.out.println("\t>>>>>>>>> Run before App\n");
+
+		if (car)
+		cardRepository.save(new Card("temoin", Category.JAVA,"Question Témoin","Réponse Témoin"));
+
+	}
 }
 
 // --spring.profiles.active=test
