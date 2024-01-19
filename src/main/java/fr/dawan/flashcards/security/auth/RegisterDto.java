7182f6d72311703ec7f9dba961711f2f46081857
Sukushi <romain.chuche@gmail.com>
2024-01-19 16:03:27 +0100

    Fix

--- src/main/java/fr/dawan/flashcards/security/auth/RegisterDto.java
+++ src/main/java/fr/dawan/flashcards/security/auth/RegisterDto.java
@@ -1,5 +1,6 @@
 package fr.dawan.flashcards.security.auth;
 
+import fr.dawan.flashcards.business.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
