7d491cb505989530f191f78bac0d8ecdbe25676e
BarbeDouce <baptiste.louerat44@gmail.com>
2024-02-12 18:04:11 +0100

    Fixes : removed id for now. compiles.

--- src/main/java/fr/dawan/flashcards/security/auth/LoginResponseDto.java
+++ src/main/java/fr/dawan/flashcards/security/auth/LoginResponseDto.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponseDto {
-    private long id;
     private UserDto user;
     private String token;
 }
